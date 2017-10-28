ab27a35ac6dae2b08a6bda4314d95eceeed768b0
Carlo <carlo.namoca@gmail.com>
2017-10-28 12:26:59 -0700

    Initial Commit

---
+++ SwiftyCats/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyCats
+//
+//  Created by Carlo Namoca on 2017-10-28.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
