d8cd0174fb89ffa5815b0cc04d704bbf4839803e
Carlo <carlo.namoca@gmail.com>
2017-10-28 16:37:49 -0700

    get collectionview to work

--- SwiftyCats/ViewController.swift
+++ SwiftyCats/ViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    @IBOutlet weak var collectionView: UICollectionView!
+    let myLayout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        setupLayout()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupLayout(){
+//        myLayout = UICollectionViewFlowLayout()
+        myLayout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+        myLayout.minimumInteritemSpacing = 10
+        myLayout.minimumLineSpacing = 10
+//        myLayout.itemSize = CGSize(width: 150, height: 150)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height: 150)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        return cell
+    }
+    
+    
 
 
 }
